95564375f9fc72eb2e9a944c43a113e9c0edb967
Alex Xiao <Alex.Xiao@auspost.com.au>
2019-10-09 12:01:22 +1100

    hot-fix

--- apply_hot_fix.sh
+++ apply_hot_fix.sh
@@ -5,9 +5,9 @@ CODE=${DEMO_CODE_DIR:-"/opt/demo_flight_code"}
 BASE=${DEMO_BASE:-"/opt/demo_data"}
 MAIN=${DEMO_DIR:-"/opt/demo_flight"}
 rm -rf $CODE
-echo "Cleaned up CODE" >> $BASE/hot_fix.log
+echo "Cleaned up $CODE" >> $BASE/hot_fix.log
 git clone https://github.com/axxiao/demo_flight.git $CODE >> $BASE/hot_fix.log 2>> $BASE/hot_fix.log
-echo "Copy to $MAIN from $CODE"
+echo "Copy to $MAIN from $CODE"  >> $BASE/hot_fix.log
 cp -rf $CODE $MAIN
 echo "Restarting"  >> $BASE/hot_fix.log
 sudo kill $(ps -ef | grep start_monitor | awk '{print $2}')
