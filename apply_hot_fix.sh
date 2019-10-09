6de23c6cf68386c4d21a3a8dfd820f6c6fac4f93
Alex Xiao <Alex.Xiao@auspost.com.au>
2019-10-09 11:34:02 +1100

    hot-fix

--- apply_hot_fix.sh
+++ apply_hot_fix.sh
@@ -4,7 +4,7 @@ date >> $DEMO_BASE/hot_fix.log
 CODE=${DEMO_CODE_DIR:-"/opt/demo_flight_code"}
 BASE=${DEMO_BASE:-"/opt/demo_data"}
 MAIN=${DEMO_DIR:-"/opt/demo_flight"}
-rm -rf PORT=$CODE
+rm -rf $CODE
 echo "Cleaned up CODE" >> $BASE/hot_fix.log
 git clone https://github.com/axxiao/demo_flight.git $CODE >> $BASE/hot_fix.log 2>> $BASE/hot_fix.log
 echo "Copy to $MAIN from $CODE"
